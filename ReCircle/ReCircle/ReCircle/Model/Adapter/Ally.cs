b71c7ae978078ba2d179e502184f62cc84c674ae
danielPulido10 <33425807+danielPulido10@users.noreply.github.com>
2020-05-30 11:58:02 -0500

    Push clases Client, Ally, Order y services

---
+++ ReCircle/ReCircle/ReCircle/Model/Adapter/Ally.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ReCircle.Model.Adapter
+{
+    public class Ally
+    {
+        public string IdAlly { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public decimal Score { get; set; }
+        public bool IsBusy { get; set; }
+    }
+}

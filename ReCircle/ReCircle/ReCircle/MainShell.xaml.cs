ff82a97ec0b2cfbbddb72c5f21f7c20076fc0f0d
Cristian Alvarado <cristianalpi@unisabana.edu.co>
2020-05-30 19:55:03 -0500

    Update MainShell.xaml.cs

--- ReCircle/ReCircle/ReCircle/MainShell.xaml.cs
+++ ReCircle/ReCircle/ReCircle/MainShell.xaml.cs
@@ -10,7 +10,7 @@ using Xamarin.Forms.Xaml;
 namespace ReCircle
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MainShell : ContentPage
+    public partial class MainShell : Shell
     {
         public MainShell()
         {
